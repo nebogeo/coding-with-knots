734c85423d9e1de37cc759ca3af73ffc0c59b63d
Dave Griffiths <dave@fo.am>
2015-05-28 00:35:43 +0100

    loads more colours and stuff

--- download.py
+++ download.py
@@ -2,10 +2,12 @@
 
 import urllib
 
-for i in range(0,32):
+for i in range(0,200):
     num = ("%03d"%i)
-    filename = "HP"+num+".xls"
+    filename = "UR1"+num+".xls"
     print(filename)
     #url = "http://khipukamayuq.fas.harvard.edu/DataTables/UR%20%3C1000%20data%20tables/"+filename
-    url = "http://khipukamayuq.fas.harvard.edu/DataTables/HP%20data%20tables/"+filename
+    url = "http://khipukamayuq.fas.harvard.edu/DataTables/UR1000%20data%20tables/"+filename
+
+    #url = "http://khipukamayuq.fas.harvard.edu/DataTables/HP%20data%20tables/"+filename
     urllib.urlretrieve (url, filename)
